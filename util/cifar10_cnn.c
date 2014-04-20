2d2960f4cb276b36a8132680c0ac5663fd5ce5f7
= <henry.gouk@gmail.com>
2014-04-21 02:51:12 +1200

    Tweaked demos.

--- util/cifar10_cnn.c
+++ util/cifar10_cnn.c
@@ -9,7 +9,7 @@
 #include "../source/dropout.h"
 
 #include "cifar10.h"
-#include "mnist.h"
+#include "evaluate.h"
 
 int main(int argc, char **argv)
 {
@@ -18,25 +18,25 @@ int main(int argc, char **argv)
 	cifar10(argv[1], &features, &labels);
 
 	layer_t *layers[10];
-	layers[0] = dropout_create(32 * 32 * 3, 1.0);
+	layers[0] = dropout_create(32 * 32 * 3, 0.8);
 	layers[1] = fftconv_create(3, 32, 32, 5, RECTIFIED, 0.02);
 	layers[2] = maxpool_create(32, 28, 2);
-	layers[3] = dropout_create(32 * 14 * 14, 1.0);
+	layers[3] = dropout_create(32 * 14 * 14, 0.8);
 	layers[4] = fftconv_create(32, 14, 32, 5, RECTIFIED, 0.02);
 	layers[5] = maxpool_create(32, 10, 2);
-	layers[6] = dropout_create(32 * 5 * 5, 1.0);
-	layers[7] = full_create(32 * 5 * 5, 100, RECTIFIED, 0.1);
-	layers[8] = dropout_create(100, 1.0);
-	layers[9] = full_create(100, 10, SOFTMAX, 0.1);
+	layers[6] = dropout_create(32 * 5 * 5, 0.8);
+	layers[7] = full_create(32 * 5 * 5, 100, RECTIFIED, 0.02);
+	layers[8] = dropout_create(100, 0.8);
+	layers[9] = full_create(100, 10, SOFTMAX, 0.02);
 
 	update_rule_t *update_rule = (update_rule_t *)malloc(sizeof(update_rule_t));
 	update_rule->algorithm = SGD | MOMENTUM;
-	update_rule->learning_rate = 0.001;
+	update_rule->learning_rate = 0.0001;
 	update_rule->momentum_rate = 0.9;
 
 	update_rule_t *full_update_rule = (update_rule_t *)malloc(sizeof(update_rule_t));
 	full_update_rule->algorithm = SGD | MOMENTUM;
-	full_update_rule->learning_rate = 0.001;
+	full_update_rule->learning_rate = 0.0001;
 	full_update_rule->momentum_rate = 0.9;
 
 	layers[1]->update_rule = update_rule;
--- util/evaluate.h
+++ util/evaluate.h
@@ -1,6 +1,6 @@
 #ifndef _EVALUATE_H_
 #define _EVALUATE_H_
 
-void evaluate(ffnn_t *ffnn, nnet_float_t *features, nnet_float_t *labels, size_t count, size_t num_features, size_t num_outputs);
+nnet_float_t evaluate(ffnn_t *ffnn, nnet_float_t *features, nnet_float_t *labels, size_t count, size_t num_features, size_t num_outputs);
 
 #endif
